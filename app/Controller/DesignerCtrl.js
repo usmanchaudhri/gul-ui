c4c34717daaa9ab7ca8dd1c90def051cdcd9b4f0
jawad khan <jawadkhan918@gmail.com>
2015-12-09 01:42:48 +0500

    First commit

--- app/Controller/DesignerCtrl.js
+++ app/Controller/DesignerCtrl.js
@@ -1,17 +1,24 @@
  app.controller('Ctrl1', function($scope,$http,$q,$timeout,$location) {
         
-        $scope.target = $location.search().target;
- 		
-		var promise3 = $http({method: 'GET', url: 'https://gul-product-service.herokuapp.com/gul-product-service/shop/'+$scope.target, cache: 'true'});
+		$scope.target = $location.search().target;
+		$http.get("url.properties")
+		.then(function(response) {
+				$http.get(response.data.shopUrl + '/' + $scope.target)
+				.then(function(response1){
+						$scope.designerProducts = response1;
+    	
+					});
+    	});
+		/*	var promise3 = $http({method: 'GET', url: 'https://gul-product-service.herokuapp.com/gul-product-service/shop/'+$scope.target, cache: 'true'});
 		
 		
 		
 		$q.all([promise3]).then(function(data){
-			$scope.designerProducts = data[0];
+		$scope.designerProducts = data[0];
 		});
-	
+		*/
    
    
-            }
-        );
+	}
+);
         
--- app/Controller/MenuCtrl.js
+++ app/Controller/MenuCtrl.js
@@ -1,20 +1,15 @@
 app.controller('MenuCtrl', function($scope, $q, $http, $timeout) {
-	
-	
-	var promise1 = $http({method: 'GET', url: 'https://gul-product-service.herokuapp.com:443/gul-product-service/product', cache: 'true'});
-	var promise2 = $http({method: 'GET', url: 'https://gul-product-service.herokuapp.com/gul-product-service/category', cache: 'true'});
-	var promise3 = $http({method: 'GET', url: 'https://gul-product-service.herokuapp.com/gul-product-service/shop', cache: 'true'});
-		
-		
-		
-		$q.all([promise1, promise2, promise3]).then(function(data){
-			$scope.product = data[0].data;
-			$scope.cat = data[1].data;
-			$scope.shop = data[2].data;
-		});
-	
-   
-   
-            }
-        );
+		$http.get("url.properties")
+		.then(function(response) {
+				var promise1 = $http({method: 'GET', url: response.data.productUrl, cache: 'true'});
+				var promise2 = $http({method: 'GET', url: response.data.categoryUrl, cache: 'true'});
+				var promise3 = $http({method: 'GET', url: response.data.shopUrl, cache: 'true'});
+				$q.all([promise1, promise2, promise3]).then(function(data){
+						$scope.product = data[0].data;
+						$scope.cat = data[1].data;
+						$scope.shop = data[2].data;
+					});
+			}
+		);
         
+	});
