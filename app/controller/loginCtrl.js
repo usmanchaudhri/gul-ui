0c4f74a30e9e7eb0dc87f429526f3ea892724416
Uxair Rahim <Uxair Rahim>
2016-03-31 12:02:22 +0500

    css changings

--- app/controller/loginCtrl.js
+++ app/controller/loginCtrl.js
@@ -26,30 +26,35 @@
 	
 	
 			$scope.userLogout = function(){
-				$cookieStore.remove("login");
-				$scope.loginUser = "logout";
-				console.log($scope.loginUser);
+				if($cookieStore.get($scope.loginEmail)!= null && $cookieStore.get($scope.loginPass)){
+					$cookieStore.remove($scope.loginEmail);
+					$cookieStore.remove($scope.loginPass);
+					$scope.userFlag = false;
+				}else{
+					$scope.userFlag = true;
+				}
+				
+				
+				console.log("Email after logout"+$cookieStore.get($scope.loginEmail));
 			};
 	
 			$scope.checkLogin = function(){
-				if($scope.loginPass != '' && $scope.loginEmail != ''){
-					$cookieStore.put("login",'login');
-					$cookieStore.put("username",$scope.loginEmail);
-					$scope.loginUser = "login";
-				/*	checkUser($scope.loginEmail);
-					*/
-				siginInUser();					
-				}
+				/*if($scope.loginPass != '' && $scope.loginEmail != ''){
+						$scope.siginInUser();					
+				}else{
+					$scope.userFlag = false;
+				}*/
+				$scope.dismiss();
 				
-				console.log("Check: "+$scope.loginUser);
+				console.log("Check: "+$scope.loginEmail);
 		
 			};
 			
 			
 			
 			var checkUser = function(email){
-				regUser($scope.loginEmail);
-				/*$http.get($scope.customerUrl)
+				
+				$http.get($scope.customerUrl)
 				.then(function(response1){
 						for(var i = 0;i< response1.data.length;i++){
 							if(response1.data[i].email == email){
@@ -60,8 +65,8 @@
 						}
 						console.log(response1);
 						
-					});		*/		
-				
+					});		
+
 			}
 			
 			$scope.regHeroku = function(){
@@ -82,19 +87,22 @@
 						regUser($scope.regEmail);
 						
 					}).error(function (data, status) {
-						console.log("Registration Data".data);
+						console.log("Registration Data"+data);
 						console.log(status);
 					});
 				
 			}
 			
-			var siginInUser = function(){
+			$scope.userFlag = false;
+			
+			$scope.siginInUser = function(){
 				
 				var base64 = Base64.encode( $scope.loginEmail + ':' + $scope.loginPass );
-				var loginAuth = 'Basic ' + base64;
+				var loginAuth =  base64;
 				var config = {
 					headers : {
-						'Authorization': loginAuth
+						'Content-Type': 'application/json',
+						'Authorization': 'Basic ' + loginAuth
 					}
 				}
 				
@@ -102,14 +110,20 @@
 				$http.get(
 					$scope.loginUrl,config
 				).success(function(data, status) {
-						console.log(data);
-						
-						
+							console.log("Email when login"+$cookieStore.get("username"));
+							
+							/*$cookieStore.put("username",$scope.loginEmail);
+							$cookieStore.put("password",$scope.loginPass);
+							
+							$scope.userFlag = true;
+							console.log("Email when login"+$cookieStore.get("username"));
+							console.log("Flag variable"+$scope.userFlag);*/
+							
 					}).error(function (data, status) {
-						console.log("Registration Data".data);
+						console.log("Login Data"+data);
 						console.log(status);
+						$scope.dismiss();
 					});
-				
 			}
 			
 			
